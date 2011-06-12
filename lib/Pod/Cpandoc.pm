ed1cb93ceafb23eca7ed34000027d43306ad0ed0
Shawn M Moore <cpan@sartak.org>
2011-06-12 00:58:35 +0000

    Import of SARTAK/Pod-Cpandoc-0.05 from CPAN.
    gitpan-cpan-distribution: Pod-Cpandoc
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SARTAK/Pod-Cpandoc-0.05.tar.gz
    gitpan-cpan-author:       SARTAK
    gitpan-cpan-maturity:     released

--- lib/Pod/Cpandoc.pm
+++ lib/Pod/Cpandoc.pm
@@ -6,7 +6,12 @@ use base 'Pod::Perldoc';
 use HTTP::Tiny;
 use File::Temp 'tempfile';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
+
+sub unlink_tempfiles {
+    my $self = shift;
+    return $self->opt_l ? 0 : 1;
+}
 
 sub scrape_documentation_for {
     my $self   = shift;
@@ -25,7 +30,11 @@ sub scrape_documentation_for {
     return unless $response->{success};
 
     $module =~ s/::/-/g;
-    my ($fh, $fn) = tempfile("${module}-XXXX", UNLINK => 1);
+    my ($fh, $fn) = tempfile(
+        "${module}-XXXX",
+        UNLINK => $self->unlink_tempfiles,
+        TMPDIR => 1,
+    );
     print { $fh } $response->{content};
     close $fh;
 
@@ -40,7 +49,11 @@ sub grand_search_init {
         my $pages = shift;
 
         for my $module (@$pages) {
-            push @found, $self->scrape_documentation_for($module);
+            my @files = $self->scrape_documentation_for($module);
+            if (@files == 0) {
+                $self->real_report_no_matches($module);
+            }
+            push @found, @files;
         }
     }
 
@@ -55,6 +68,21 @@ sub opt_V {
     return $self->SUPER::opt_V(@_);
 }
 
+# these next two functions attempt to cover Pod-Perldoc versions
+# with and without my report_no_matches patch
+sub real_report_no_matches {
+    my $self = shift;
+
+    # if SUPER::can('report_no_matches') fails, then we have already reported
+    # no matches before even trying to scrape CPAN
+    if ($self->SUPER::can('report_no_matches')) {
+        $self->SUPER::report_no_matches(@_);
+    }
+}
+
+# don't report_no_matches until we've tried scraping CPAN
+sub report_no_matches { }
+
 1;
 
 __END__
@@ -64,6 +92,9 @@ __END__
 Pod::Cpandoc - perldoc that works for modules you don't have installed
 
 =head1 SYNOPSIS
+
+    cpandoc File::Find
+        -- shows the documentation of your installed File::Find
 
     cpandoc Acme::BadExample
         -- works even if you don't have Acme::BadExample installed!
@@ -76,6 +107,25 @@ Pod::Cpandoc - perldoc that works for modules you don't have installed
 
     vim `cpandoc -l Web::Scraper`
         -- getting the idea yet?
+
+=head1 DESCRIPTION
+
+C<cpandoc> is a perl script that acts like C<perldoc> except that
+if it would have bailed out with
+C<No documentation found for "Uninstalled::Module">, it will instead
+scrape a CPAN index for the module's documentation.
+
+One important feature of C<cpandoc> is that it I<only> scrapes the
+live index if you do not have the module installed. So if you use
+C<cpandoc> on a module you already have installed, then it will
+just read the already-installed documentation. This means that the
+version of the documentation matches up with the version of the
+code you have. As a fringe benefit, C<cpandoc> will be fast for
+modules you've installed. :)
+
+All this means that you should be able to drop in C<cpandoc> in
+place of C<perldoc> and have everything keep working. See
+L</SNEAKY INSTALL> for how to do this.
 
 =head1 SNEAKY INSTALL
 
@@ -94,6 +144,8 @@ it fails to find your queried file in C<@INC>.
 =head1 SEE ALSO
 
 The sneaky install was inspired by L<https://github.com/defunkt/hub>.
+
+http://tech.bayashi.jp/archives/entry/perl-module/2011/003305.html
 
 =head1 AUTHOR
 
